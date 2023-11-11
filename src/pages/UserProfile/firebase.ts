2a79b5703d7ad9cc3e2d9b3bb4ac785a71c89133
Công Tuấn (TiT) <congtuan0104@gmail.com>
2023-11-11 11:42:50 +0700

    firebase facebook login

--- src/pages/UserProfile/firebase.ts
+++ src/pages/UserProfile/firebase.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { FacebookAuthProvider, getAuth, GoogleAuthProvider } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA98sO_BtFdpH4yl_TPo-DfNIa6Q3r_9co",
-  authDomain: "login-6752e.firebaseapp.com",
-  projectId: "login-6752e",
-  storageBucket: "login-6752e.appspot.com",
-  messagingSenderId: "931199521045",
-  appId: "1:931199521045:web:37a2732ed858b675e0d91c",
-  measurementId: "G-7JLRRQJENS"
+  apiKey: 'AIzaSyDLE4z4TMB5SsM9RqSxcqj6iUwTT6TFGus',
+  authDomain: 'clinic-admin-e5eae.firebaseapp.com',
+  projectId: 'clinic-admin-e5eae',
+  storageBucket: 'clinic-admin-e5eae.appspot.com',
+  messagingSenderId: '713822273863',
+  appId: '1:713822273863:web:15b2f34ade2fb01ecf4c62',
+  measurementId: 'G-JLE3CG0FJQ',
 };
 
 // Initialize Firebase
@@ -22,7 +22,8 @@ const auth = getAuth(app);
 //var provider = new firebase.auth.GoogleAuthProvider();
 
 const provider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 provider.setCustomParameters({
-  prompt: "select_account"
+  prompt: 'select_account',
 });
-export { auth, provider };
+export { auth, provider, facebookProvider };
