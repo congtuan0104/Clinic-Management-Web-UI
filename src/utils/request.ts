bb2ad827a963d6a06b439f41ebc80016abe66e9d
Công Tuấn (TiT) <congtuan0104@gmail.com>
2023-11-19 22:14:41 +0700

    test

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,7 +7,7 @@ export const REQUEST_TIMEOUT = 30000;
 
 export const axiosClient = axios.create({
   // host api được cấu hình trong vite.config.ts -> thay đổi theo env
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `/api`,
   timeout: REQUEST_TIMEOUT,
 });
 
