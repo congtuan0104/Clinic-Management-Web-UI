c0f6217fdd46577f9171038dfd2296f4857a5a99
Công Tuấn (TiT) <congtuan0104@gmail.com>
2023-11-22 00:29:59 +0700

    fix logo

--- src/components/Header/DefaultHeader.tsx
+++ src/components/Header/DefaultHeader.tsx
@@ -24,6 +24,7 @@ import {
 
 import { useDisclosure } from '@mantine/hooks';
 import { Link } from 'react-router-dom';
+import ClinusLogo from '@/assets/images/logo-and-title.png'
 
 
 // header cho các trang dành cho khách hàng chưa đăng nhập
@@ -39,7 +40,7 @@ const Header = () => {
         <Group justify="space-between" h="100%">
           <Box w={userInfo ? undefined : 222}>
             <Link to={PATHS.HOME}>
-              <Image src='/src/assets/images/logo.png' alt='logo' h={50} fit='contain' />
+              <Image src={ClinusLogo} alt='logo' h={50} fit='contain' />
             </Link>
           </Box>
 
