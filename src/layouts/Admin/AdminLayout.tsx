b1044ef191f4d5099008db2c27313e690b4218d2
Công Tuấn (TiT) <congtuan0104@gmail.com>
2023-12-07 00:47:03 +0700

    chore

--- src/layouts/Admin/AdminLayout.tsx
+++ src/layouts/Admin/AdminLayout.tsx
@@ -1,13 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 
 import { AdminHeader, DefaultHeader } from '@/components';
+import { Box } from '@mantine/core';
 
 const AdminLayout = ({ children }: { children: JSX.Element }) => {
   return (
-    <div>
+    <Box mih='100%'>
       <AdminHeader />
       <main className="max-w-screen-xxl mx-auto min-h-[calc(100vh_-_60px)]">{children}</main>
-    </div>
+    </Box>
   );
 };
 
