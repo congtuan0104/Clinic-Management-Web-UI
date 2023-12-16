5afa891bde52c3100825778ec4ae8a785a9b8896
anvo1405 <vohoaian62@gmail.com>
2023-12-16 10:31:56 +0700

    UI tạo nhóm chat

--- src/pages/Message/ChatScreen.tsx
+++ src/pages/Message/ChatScreen.tsx
@@ -3,26 +3,23 @@ import React, { useEffect, useState } from "react";
 import GroupChatList from "@/components/Message/GroupChatList";
 import Conversation from "@/components/Message/Conversation";
 
-import { IGroupChat } from "@/types"
-//import { db } from "../../firebase";
+import { IGroupChat } from "@/types";
+// import { db } from "../../firebase";
 
 import "./chat-screen.css";
 import { useAuth } from "@/hooks";
 import { GroupChatType } from "@/enums";
 
-import { Button, Text } from "@mantine/core";
-import { useDocumentTitle } from "@mantine/hooks";
-
-// get user from redux
-
+import { Button, Text, Modal, TextInput, Select, MultiSelect, Avatar, ModalRoot, ModalHeader, ModalCloseButton, ModalBody } from "@mantine/core";
+import { useDocumentTitle, useDisclosure } from "@mantine/hooks";
 
 export default function ChatScreen() {
   const { userInfo } = useAuth();
-  useDocumentTitle('Clinus - Nhắn tin');
+  useDocumentTitle("Clinus - Nhắn tin");
 
-
-  const [groupChats, setGroupChats] = useState<IGroupChat[]>([]); // danh sách các nhóm chat của user hiện tại
-  const [selectedGroup, setSelectedGroup] = useState<IGroupChat | undefined>(undefined);  // nhóm chat được chọn để hiển thị
+  const [groupChats, setGroupChats] = useState<IGroupChat[]>([]);
+  const [selectedGroup, setSelectedGroup] = useState<IGroupChat | undefined>(undefined);
+  const [opened, { open, close }] = useDisclosure(false);
 
   const fetchGroupChatsByUser = () => {
     const userId = userInfo?.id;
@@ -51,7 +48,7 @@ export default function ChatScreen() {
     ];
 
     setGroupChats(fakeDataGroup);
-  }
+  };
 
   useEffect(() => {
     fetchGroupChatsByUser();
@@ -59,31 +56,69 @@ export default function ChatScreen() {
 
   const changeGroup = (group: IGroupChat) => {
     setSelectedGroup(group);
-  }
+  };
 
   return (
     <div className="p-5 h-screen overflow-auto">
       <div className="flex justify-between items-center mb-4">
-        <Text size={'22px'} fw={700}>Tin nhắn</Text>
+        <Text size={"22px"} fw={700}>
+          Tin nhắn
+        </Text>
 
-        <Button variant="filled">Tạo nhóm chat</Button>
+        <Button variant="filled" onClick={open}>
+          Tạo nhóm chat
+        </Button>
       </div>
       <div className="bg-white p-4 rounded-xl flex border border-solid border-gray-300 h-[94%]">
-        {/* Danh sách nhóm chat */}
         <div className="w-[25%] border-r border-0 border-solid border-gray-300 pr-3 bg-white max-h-full overflow-auto">
           <GroupChatList groups={groupChats} changeGroup={changeGroup} selectedGroup={selectedGroup} />
         </div>
 
-        {/* Nội dung đoạn chat*/}
         <div className="flex-1 flex flex-col">
-          {selectedGroup ? <Conversation groupChat={selectedGroup} /> :
+          {selectedGroup ? (
+            <Conversation groupChat={selectedGroup} />
+          ) : (
             <div className="flex items-center justify-center h-full">
               <p className="text-2xl text-gray-400">Chọn một nhóm chat để bắt đầu trò chuyện</p>
             </div>
-          }
+          )}
         </div>
       </div>
 
+
+
+      <Modal.Root opened={opened} onClose={close} centered size={'lg'}>
+        <Modal.Overlay />
+        <Modal.Content>
+          <ModalHeader>
+            <Modal.Title fz={22} fw={700}>Tạo nhóm</Modal.Title>
+            <ModalCloseButton />
+          </ModalHeader>
+          <ModalBody>
+            <TextInput label="Tên nhóm" placeholder="Nhập vào tên nhóm" />
+
+            <MultiSelect
+              label="Thành viên"
+              data={['Võ Hoài An', 'Phan Tài Nhật Minh', 'Nguyễn Nhật Khang', 'Phan Công Tuấn', 'Nguyễn Sơn Bão', 'Nguyễn Hải Nhật Minh']}
+              searchable
+              hidePickedOptions
+              pt={10}
+            />
+            <div style={{ display: 'flex', justifyContent: 'flex-end', }}>
+              <Button mt="lg" radius="sm" size="md" type="submit">
+                Xác nhận
+              </Button>
+              <Button mt="lg" ml="sm" radius="sm" size="md" variant='outline' color='red.5'
+                onClick={() => {
+                  close();
+                }}>
+                Hủy
+              </Button>
+            </div>
+
+          </ModalBody>
+        </Modal.Content>
+      </Modal.Root>
     </div>
   );
 }
