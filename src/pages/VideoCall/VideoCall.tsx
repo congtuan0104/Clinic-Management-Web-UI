852fddc68360a738cc86157bdc1657e72fbc190e
nhatminh287 <nhatminhpro2001@gmail.com>
2023-12-20 16:19:24 +0700

    transfer to Agora

--- src/pages/VideoCall/VideoCall.tsx
+++ src/pages/VideoCall/VideoCall.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import AgoraUIKit from 'agora-react-uikit';
 import { useAuth } from "@/hooks";
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
 
 export default function VideoCall() {
   const { userInfo } = useAuth();
@@ -10,45 +10,17 @@ export default function VideoCall() {
   const groupID = location.state?.groupID;
   const navigate = useNavigate();
 
-  let myMeeting = async (element: HTMLDivElement) => {
-    // generate Kit Token
-    const appID = 1036177780;
-    const serverSecret = "f856da2571d3cbfce4cbc3f6257d0e96";
-    const userId = userInfo?.id ? userInfo.id : '';
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, groupID.toString(), userId, userInfo?.lastName);
-
-
-    // Create instance object from Kit Token.
-    const zp = ZegoUIKitPrebuilt.create(kitToken);
-    // start the call
-    zp.joinRoom({
-      container: element,
-      sharedLinks: [
-        {
-          name: 'Personal link',
-          url:
-            window.location.protocol + '//' +
-            window.location.host + window.location.pathname +
-            '?roomID=' +
-            groupID,
-        },
-      ],
-      scenario: {
-        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-      },
-      showPreJoinView: false,
-      showLeavingView: true,
-      onLeaveRoom: () => { navigate('/clinic/messages') }
-    });
-
-
+  const rtcProps = {
+    appId: 'a188e47a27f846e68dfd23fa9e550ea8', 
+    channel: groupID.toString(), 
+    
   };
-
+  const callbacks = {
+    EndCall: () => navigate('/clinic/messages'),
+  };
   return (
-    <div
-      className="myCallContainer"
-      ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
-    ></div>
-  );
+    <div style={{display: 'flex', width: '100vw', height: '100vh'}}>
+      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+    </div>
+  ) 
 }
