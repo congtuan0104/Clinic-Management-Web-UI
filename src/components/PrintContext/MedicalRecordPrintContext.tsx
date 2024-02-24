f6aa28a9655d0f45a5b57e94b29358cfe3237b76
anvo1405 <vohoaian62@gmail.com>
2024-02-25 00:18:45 +0700

    thêm các trường thông tin medical record pdf

--- src/components/PrintContext/MedicalRecordPrintContext.tsx
+++ src/components/PrintContext/MedicalRecordPrintContext.tsx
@@ -1,6 +1,8 @@
 import { IMedicalRecord } from "@/types";
 import { forwardRef } from "react";
-import { Text } from "@mantine/core";
+import { Center, Divider, Grid, GridCol, Group, Stack, Table, Text } from "@mantine/core";
+import dayjs from "dayjs";
+import { Gender } from "@/enums";
 
 interface IProps {
   record: IMedicalRecord;
@@ -28,6 +30,105 @@ const MedicalRecordPrintContext = forwardRef<HTMLDivElement, IProps>((props, ref
       <b>Số hồ sơ: </b>{record?.id}
     </Text>
 
+    <Grid py={10}>
+      <GridCol span={6}>
+          <Group>
+          <Text>Họ tên: {`${record.patient.firstName} ${record.patient.lastName}`}</Text>
+          </Group>
+      </GridCol>
+      <GridCol span={6} >
+      <Text ta='center'>Tuổi: {record.patient?.birthday
+                      ? dayjs().diff(record.patient?.birthday, 'year')
+                      : 'Chưa có thông tin'}</Text>
+      </GridCol>
+      <GridCol span={6}>
+      <Text>Giới tính: {record?.patient.gender === Gender.Male ? 'Nam'
+                      : record?.patient.gender === Gender.Female ? 'Nữ' : 'Chưa có thông tin'}</Text>
+      </GridCol>
+      <GridCol span={6}>
+      <Text>Ngày sinh: {record.patient.birthday ? dayjs(record.patient.birthday).format('DD/MM/YYYY') : 'Chưa có thông tin'}</Text>
+      </GridCol>
+      <GridCol span={6}>
+      <Text>CMND/CCCD: {record.patient.idCard ? record.patient.idCard : 'Chưa có thông tin'}</Text>
+      </GridCol>
+      <GridCol span={6}>
+      <Text>Số thẻ bảo hiểm y tế: {record.patient.healthInsuranceCode ? record.patient.healthInsuranceCode : 'Chưa có thông tin'}</Text>
+      </GridCol>
+      <GridCol span={12}>
+      <Text>Địa chỉ: {record.patient.address ? record.patient.address : 'Chưa có thông tin'}</Text>
+      </GridCol>
+      <GridCol span={12}>
+      <Text>Số điện thoại: {record.patient.phone ? record.patient.phone : 'Chưa có thông tin'}</Text>
+      </GridCol>    
+      <GridCol span={12}>
+      <Text>Email: {record.patient.email ? record.patient.email : 'Chưa có thông tin'}</Text>
+      </GridCol> 
+      <GridCol span={12}>
+    <Table withTableBorder withColumnBorders>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Chiều cao (cm)</Table.Th>
+          <Table.Th>Cân nặng (kg)</Table.Th>
+          <Table.Th>Huyết áp (mmHg)</Table.Th>
+          <Table.Th>Nhiệt độ (°C)</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>
+      <Table.Tr>
+      <Table.Td>{record.height}</Table.Td>
+      <Table.Td>{record.weight}</Table.Td>
+      <Table.Td>{record.bloodPressure}</Table.Td>
+      <Table.Td>{record.temperature}</Table.Td>
+    </Table.Tr>
+      </Table.Tbody>
+    </Table>
+    </GridCol>
+    <GridCol span={12}>
+      <Group>
+      <Text fw={700}>Dịch vụ:</Text>
+      {record.medicalRecordServices.map((service) => (
+        <Text>{service.serviceName},</Text>
+      ))}
+      </Group>
+      
+    </GridCol>
+    <GridCol span={12}>
+      <Group>
+      <Text fw={700}>Chẩn đoán:</Text>
+      <Text>{record.diagnose}</Text>
+      </Group>
+
+    </GridCol>
+    <GridCol span={12}>
+    <Group>
+      <Text fw={700}>Kết luận:</Text>
+      <Text>{record.result}</Text>
+      </Group>
+    </GridCol>
+    <GridCol span={12}>
+      <Text fw={700}>Toa thuốc: </Text>
+      {record.prescriptionDetail.map((medicine) => (
+        <Text>{medicine.medicineName} : {medicine.dosage} {medicine.unit} ({medicine.duration} - {medicine.doseInterval} - {medicine.usingTime}) </Text>
+      ))}
+    </GridCol>
+    <GridCol span={12}>
+      <Stack align="flex-end">
+        <Stack align="flex-start">
+        <Text>Ngày khám: {dayjs(record.dateCreated).format('DD/MM/YYYY')}</Text>
+      <Text>Bác sĩ: {record.doctor.firstName} {record.doctor.lastName}</Text>
+        </Stack>
+      
+      </Stack>
+
+    </GridCol>
+    
+    </Grid>
+
+    
+
+          
+       
+    
 
   </div>
 })
