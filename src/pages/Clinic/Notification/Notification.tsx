068016b3bd2160a3b5ba0bd38a4669e61ca13775
anvo1405 <vohoaian62@gmail.com>
2024-03-12 22:36:36 +0700

    fix đảo danh sách thông báo khi chuyển trang

--- src/pages/Clinic/Notification/Notification.tsx
+++ src/pages/Clinic/Notification/Notification.tsx
@@ -13,6 +13,7 @@ import { renderSendingTime } from '@/utils';
 import { realtimeDB } from '@/config';
 import { onValue, ref } from 'firebase/database';
 import { useAuth } from '@/hooks';
+import { IoMdNotifications } from "react-icons/io";
 
 const NotificationPage = () => {
     const [notify, setNotify] = useState<INotification[]>([]);
@@ -30,24 +31,35 @@ const NotificationPage = () => {
     }, [userInfo?.id])
     return (
         <div className="mt-6 ml-4 mr-4">
-            <Group justify='space-between'>
-                <Text fw={700} pb={15} size='20px'>Thông báo</Text>
-            </Group>
-            <div className='flex flex-col bg-white p-3 rounded-xl border border-solid border-gray-300'>
-            {notify.length === 0 ? <p>Bạn không có thông báo nào</p> : <></>}
-            {notify.reverse().map((item) => (
-                <div key={item.id}
-                    className="flex cursor-pointer p-[6px] rounded-md flex-col group hover:bg-gray-200">
-                    <div className='border-solid border-0 border-l-[3px] border-primary-300 pl-2'>
-                        <p className='text-14 text-gray-700 group-hover:text-black-90 text-justify mt-1'>{item.content}</p>
-                        <p className='text-gray-500 text-13'>{renderSendingTime(item.sendingTime)}</p>
-                    </div>
+<Group align='center' mb={10}>
+  <IoMdNotifications size='30px' />
+  <Text fw={700} size='20px'>Thông báo</Text>
+</Group>
+          <div className='flex flex-col bg-white p-3 rounded-xl border border-solid border-gray-300'>
+            {notify.length === 0 ? (
+              <p>Bạn không có thông báo nào</p>
+            ) : (
+              notify.map((item, index) => (
+                <div
+                  key={item.id}
+                  className={`flex cursor-pointer p-[6px] rounded-md flex-col group ${
+                    index % 2 === 0 ? 'bg-gray-200' : ''
+                  }`} // Apply background color based on index
+                >
+                  <div className='border-solid border-0 border-l-[3px] border-primary-300 pl-2'>
+                    <p
+                      className='text-14 text-gray-700 group-hover:text-black-90 text-justify mt-1'
+                    >
+                      {item.content}
+                    </p>
+                    <p className='text-gray-500 text-13'>{renderSendingTime(item.sendingTime)}</p>
+                  </div>
                 </div>
-            ))}
-            </div>
-            
+              ))
+            ).reverse()}
+          </div>
         </div>
-    )
+      );
 };
 
 export default NotificationPage;
